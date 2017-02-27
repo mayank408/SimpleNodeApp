5d55abc1fe14394d72ae1960ff06baaeab22d97d
Mayank <mayank.trp48@gmail.com>
2017-02-28 00:25:06 +0530

    app.js updated

---
+++ controllers/todoController.js
@@ -0,0 +1,25 @@
+module.exports = function(app){
+
+	app.get('/todo' , function(req,res){
+
+
+
+	})
+
+
+
+	app.post('/todo' , function(req,res){
+
+		
+		
+	})
+
+
+
+	app.delete('/todo' , function(req,res){
+
+		
+		
+	})
+
+}
