c35c2de45240ef4db967ce498496a7820fc7be12
Mayank <mayank.trp48@gmail.com>
2017-03-13 17:34:01 +0530

    mongodb updated

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -10,43 +10,40 @@ var todoschema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo',todoschema)
 
-var itemOne = Todo({item:'Buy flowers'}).save(function(err){
-	if(err) throw err;
-	console.log('item saved');
-})
-
-var data = [{item: 'get milk'} ,{item:'walk dog'} , {item:'kick some codin'}]
+//var data = [{item: 'get milk'} ,{item:'walk dog'} , {item:'kick some codin'}]
 var url = bodyParser.urlencoded({extended: false})
 
 module.exports = function(app){
 
 	app.get('/todo' , function(req,res){
 
-		res.render('todo',{todos: data})
+		Todo.find({} , function(err,data){
+			if(err) throw err;
+			res.render('todo',{todos: data})
 
-
+		})
 	})
-
 
 
 	app.post('/todo' , url, function(req,res){
 
+		var newTodo = Todo(req.body).save(function(err,data){
+			if(err) throw err
+				res.json(data);
+		})
 		console.log('request made')
-		data.push(req.body);
-		res.json(data);
-		
 	})
 
 
 
 	app.delete('/todo/:item' , function(req,res){
 
-		data = data.filter(function(todo){
-			return todo.item.replace(/ /g , '-')!== req.params.item;
-
+		Todo.find({item: req.params.item.replace(/\-/," ")}).remove(function(err,data){
+			if(err) throw err
+				res.json(data);
 		})
-		res.json(data)
-		
-	})
+
+		})		
+
 
 }
